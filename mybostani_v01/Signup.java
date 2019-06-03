e69cafdac710ec13de4465899470233fbcc171df
mybostani <50630833+mybostani@users.noreply.github.com>
2019-06-03 14:50:21 +0300

    Delete Signup.java

--- mybostani_v01/Signup.java
+++
@@ -1,8 +0,0 @@
-public class Signup {
- 
-    public static boolean authenticate(String username, String password, String email) {
-            //gia tis anagkes tou project den balame periorismo sto ti tha plhktrologhsei o xrhsths
-            return true;
-        
-    }
-}
